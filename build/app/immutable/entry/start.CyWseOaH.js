98cab55997d2580ca365440d691724ea3c7d16cc
KevinWh0 <45321184+ChaoticLeah@users.noreply.github.com>
2024-10-20 17:10:37 +0200

    fix

---
+++ build/app/immutable/entry/start.CyWseOaH.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CjeGOqkg.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@ test -->
 					<div
 						class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-primary via-secondary to-accent rounded-full blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
 					</div>
-					<a href="#projects" title="Get quote now"
+					<a href="#projects" title="See my projects"
 						class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-accent-content transition-all duration-200 glass font-pj rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary "
 						role="button">My Projects
 					</a>
